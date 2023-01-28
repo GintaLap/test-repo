60a46d04aff47967f0b3210239efa897ac176d9d
ginta.lapinska <ginta.lapinska@gmail.com>
2023-01-28 13:19:59 +0000

    Ierakstiju NASA API key

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "s3yDwMCCdT9NUJeDxddRDYdnJ0JuY4G3JOqwt1t9"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
